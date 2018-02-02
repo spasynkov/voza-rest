6014e09a042d99d1b2a0d7205f825ed01bc62eb2
spasynkov <s.pasynkov@gmail.com>
2018-02-02 19:21:49 +0200

    Hot fix: EnableWebMvc annotation added

--- src/main/java/net/ukrtel/ddns/ff/configs/WebConfig.java
+++ src/main/java/net/ukrtel/ddns/ff/configs/WebConfig.java
@@ -2,8 +2,10 @@ package net.ukrtel.ddns.ff.configs;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
+@EnableWebMvc
 @ComponentScan(basePackages = {"net.ukrtel.ddns.ff.web"})
 public class WebConfig {
 }
